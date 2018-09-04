78f684ceb285ab62a682555f43b191743cb564e7
Abhinav Agnihotri <40638746+abhiagnihotri01@users.noreply.github.com>
2018-09-05 01:33:40 +0530

    Add files via upload

---
+++ views/basad/header.php
@@ -0,0 +1,20 @@
+	<div class="row navbar-fixed">
+		<nav class="nav ">
+			<div class="nav-wrapper col s12">
+				<div class="col s4">
+					<a href="#" class="brand-logo"><i class="material-icons left">language</i>Guide</a>
+				</div>
+				<div class="col offset-s2 s4 input-field black-text">
+					<input id="search" type="search" placeholder="Search a venue or city" required>
+					<label class="label-icon" for="search"><i class="material-icons">search</i></label>
+					<i class="material-icons">close</i>
+  				</div>
+				<div class="col offset-s2 s4 navlinks">
+					<ul id="nav-mobile" class="right hide-on-med-and-down">
+						<li><a href="login.html" class="navlinks">Login</a></li>
+						<li><a href="register.html" class="navlinks">Register</a></li>
+					</ul>					
+				</div>
+			</div>
+		</nav>
+	</div>
---
+++ views/basad/index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+	<style type="text/css">
+		.nav {
+			background-color: #1E2D2F !important;
+		}
+		.searchbar {
+			background-color: white;
+		}
+		.navlinks {
+			font-size: 20px;
+		}
+	</style>
+	<title>YourGuide</title>
+</head>
+<body>
+	<?php
+		include_once("header.php");
+	?>
+</body>
+</html>
